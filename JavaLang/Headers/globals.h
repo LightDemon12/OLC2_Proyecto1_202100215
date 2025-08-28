5072ff22ac8d4939fda3d033ed56a096704eed14
Lightdemon <3870961320101@ingenieria.usac.edu.gt>
2025-08-27 21:29:12 -0600

    Bison - Flex
    Solución errores de variables, se  modifico el Cmake para crear mejor el ejecutable

---
+++ JavaLang/Headers/globals.h
@@ -0,0 +1,8 @@
+//
+// Created by lightdemon on 27/08/25.
+//
+
+#ifndef JAVALANG_GLOBALS_H
+#define JAVALANG_GLOBALS_H
+
+#endif //JAVALANG_GLOBALS_H
---
+++ JavaLang/Utils/globals.c
@@ -0,0 +1,3 @@
+//
+// Created by lightdemon on 27/08/25.
+//
